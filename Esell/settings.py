f02c636ed94ce3700dc8a2c35f8f8847061bd628
vaibhavdata <vaibhavmc01@gmail.com>
2023-04-12 19:29:29 +0530

    databaseadd

--- Esell/settings.py
+++ Esell/settings.py
@@ -77,14 +77,12 @@ WSGI_APPLICATION = 'Esell.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
